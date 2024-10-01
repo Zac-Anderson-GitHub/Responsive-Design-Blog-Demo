1687fab1f19119e440f2b660a1c9697822e1f471
Zaccypoo <135307968+Zaccypoo@users.noreply.github.com>
2024-10-01 22:16:34 +1000

    Removed vite plugin and simplified build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
-    viteStaticCopy({
-      targets: [
-        { src: 'about.html', dest: '' },
-        { src: 'article.html', dest: '' },
-      ],
-    }),
-  ],
+    // Remove the viteStaticCopy code
+  ]
 });
