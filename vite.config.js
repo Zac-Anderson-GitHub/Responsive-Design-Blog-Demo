8442371bb52ddcc276addaca5116e4edab1cae48
Zaccypoo <135307968+Zaccypoo@users.noreply.github.com>
2024-10-01 22:28:26 +1000

    Updated Vite config to handle multiple HTML files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  plugins: [
-    // Remove the viteStaticCopy code
-  ]
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        article: resolve(__dirname, 'article.html')
+      }
+    }
+  }
 });
